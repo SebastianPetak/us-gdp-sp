1ea7041515dc43d3b6bb83667f0df14591657dfa
SebastianPetak <sebastian.petak@gmail.com>
2017-03-06 15:25:17 -0600

    HomeSpec.js: Application has no testing environment. Install jasmine, d3, bluebird, react-router, winston, and d3.promise. Initialize jasmine and use d3.promise to load the us-gdp data into the usGdpResponse variable. Write a single test expecting the usGdpResponse variable to be defined.

---
+++ spec/HomeSpec.js
@@ -0,0 +1,25 @@
+var winston = require('winston');
+global.Promise = require('bluebird');
+var d3 = require('d3');
+var d3Promise = require('d3.promise');
+
+describe('Home', function() {
+	'use strict';
+	var usGdpResponse;
+
+	beforeEach(function(done) {
+		d3Promise.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+		.then(function(data) {
+			usGdpResponse = data;
+			done();
+		}, function(error) {
+			winston.log('error', error);
+			done();
+		});
+	})
+
+	it('has loaded data into it\'s testing environment', function (done) {
+		expect(usGdpResponse).toBeDefined();
+		done();
+	})
+})
